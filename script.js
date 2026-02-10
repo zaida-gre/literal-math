2c00aebebb9cc1ee4be848cf5a290a9fb6c90db2
zaida-gre <zaida.greer@stu.barren.kyschools.us>
2026-02-10 10:34:11 -0600

    practiced math stuff idk

---
+++ script.js
@@ -0,0 +1,12 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+let result = warmHugs.replace(/like/g, "love");
+let beenImpaled = "Oh, look at that. I've been impaled";
+console.log(warmHugs);
+console.log(beenImpaled.substring(17, 36));
+let dotDotDot = "...";
+let skullBones = `I don't have a skull ${dotDotDot} or bones`;
+console.log(skullBones);
+console.log(Math.PI);
+let randomNumber = Math.floor(Math.random()*3 +1);
+console.log(randomNumber);
